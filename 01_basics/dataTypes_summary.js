914497b00630e4d28dc745c084b4706c2aa215be
Sukhwinder Singh <134933892+Sukhwinder-i0@users.noreply.github.com>
2024-04-25 08:12:45 +0000

    data type summary

---
+++ 01_basics/dataTypes_summary.js
@@ -0,0 +1,44 @@
+// 2 types - PRIMITIVE AND NON-PRIMITIVE
+
+// (1) PRIMITIVE :-- (call by values, changes we do happens in copy not in original0)
+
+// 7 types :: String, Number, Boolean, null, undefined, Symbol(to make unique a value), BigInt
+
+
+
+/* ((((( DYNAMICALLY TYPED 
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLogged = false
+// const outsideTemp = null;
+// let userEmail;
+
+))))))))) */
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId)
+
+
+
+const bigNumber =595956265999569262655n // stored in bigInt 
+
+
+
+
+
+
+// (2) REFERENCE( non primitive ) 
+
+// to master js master OBJECTS and BROWSER WEB EVENTS or BROWSER EVENTS
+
+// Arrays, Objects, Functions
+
+let myObj = {
+    name: "Sukhwinder Singh",
+    age: 19,
+}
+
