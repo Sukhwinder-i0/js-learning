7aa11f689971b980676b12a104a1c144af0b82f1
Sukhwinder Singh <134933892+Sukhwinder-i0@users.noreply.github.com>
2024-05-01 08:31:01 +0000

    arrays

--- 02_basics/01_arrays.js
+++ 02_basics/01_arrays.js
@@ -5,9 +5,43 @@ const myArr0 = new Array(9, 5, 4, 2, "Sukhwinder Singh")            // method -2
 
 // array-copy-operations create shallow copies ( whose properies share the same refrence point) // Deep copy -->> vice versa
 
-console.log(myArr[4]);
-
+// console.log(myArr[4]);
 
 
 // *********** ARRAY METHODS  *************//
+
+// myArr.push(9, 1008)             // adds from last
+// myArr.pop()                     // deletes from last
+
+
+// myArr.unshift(34, 19)         // adds at start     
+// myArr.shift()                 // deletes from start
+
+// console.log(myArr.includes("singh"));
+// console.log(myArr.indexOf("Sukhwinder"));
+
+// const newArr = myArr.join();           // adds all the element into string
+// console.log(newArr);
+// console.log(typeof newArr);
+
+// console.log(myArr);
+
+
+// ******** SLICE and SPLICE  ***********//
+
+console.log("A ", myArr);
+
+// slice don't change in original array
+
+const myn1 = myArr.slice(1, 3)
+console.log(myn1);
+console.log("B ", myArr);
+
+
+// splice do change in original array 
+
+const myn2 = myArr.splice(1,3)
+console.log(myn2);
+console.log("C ", myArr);
+
 
