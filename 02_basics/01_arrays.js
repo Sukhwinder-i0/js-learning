43f531bb2e0315007093c348b4c480cc3fcebb78
Sukhwinder Singh <134933892+Sukhwinder-i0@users.noreply.github.com>
2024-05-01 07:39:12 +0000

    arrays intro

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,13 @@
+// ***********  ARRAYS  ******************//
+
+const myArr = [0, 4, 6, 8, "Sukhwinder"]                             // method - 1 // prototype access inside prototype
+const myArr0 = new Array(9, 5, 4, 2, "Sukhwinder Singh")            // method -2
+
+// array-copy-operations create shallow copies ( whose properies share the same refrence point) // Deep copy -->> vice versa
+
+console.log(myArr[4]);
+
+
+
+// *********** ARRAY METHODS  *************//
+
