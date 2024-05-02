85f6baa0e03693311fa7cfdbeb092524b2f18f1b
Sukhwinder Singh <134933892+Sukhwinder-i0@users.noreply.github.com>
2024-05-02 09:29:49 +0000

    objects2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,77 @@
+// const tinderUser = new Object()   // singleton object <<----
+
+const tinderUser = {}   // non  singleton object
+
+// BOTH are SAME returns empty object
+
+
+tinderUser.id = "xyz899"
+tinderUser.name = "ABC"
+
+
+
+const regularUser = {
+    email: "Sukhwinder@gitHub.com",
+    fullname: {
+        userfullname: {
+            firstname: "Sukhwinder",
+            lastname: "Singh"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname);
+
+// if a value doesn't exist them use ?
+// console.log(regularUser.fullname?.userfullname.firstname);
+
+
+
+// ************  MERGING of objects  *************//
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "c", 4: "d"}
+// const obj3 = {obj1, obj2}    // -->> PROBLEM -- objects comes in object 
+
+// const obj3 = Object.assign(obj1, obj2)     // it also fine
+// const obj3 = Object.assign({}, obj1, obj2)    // good practice
+// console.log(obj3);
+
+/* Object.assign(Target, Source)
+that's why we take an empty object as a target */
+
+
+const obj0 = {...obj1, ...obj2}     // used in 90% cases
+console.log(obj0);
+
+
+
+// values came from database in form of array separated by commas
+// OBJECTS in array
+
+const user = [
+    {
+        id: 1,
+        email: "user1abc"
+    },
+    {
+        id: 2,
+        email: "user2abc"
+    },
+    {
+        id: 3,
+        email: "user3abc"
+    }
+]
+
+console.log(user[1].email)
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));   // PUTS all the KEYS in ARRAY that can be used further
+console.log(Object.values(tinderUser));
+console.log(Object.entries(tinderUser));  // Makes arrays of keys and values 
+
+// console.log(typeof Object.keys(tinderUser));  //doesn't change it's type
+
+console.log(tinderUser.hasOwnProperty('Name'));
+console.log(tinderUser.hasOwnProperty('id'));
