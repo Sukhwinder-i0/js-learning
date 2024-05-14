9a62eaedabb610cec9bdb5cb6147b974eefb10e1
Sukhwinder Singh <134933892+Sukhwinder-i0@users.noreply.github.com>
2024-05-14 01:59:19 +0000

    control flow

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,36 @@
+// switch (key) {
+//     case value:
+        
+//         break;          [SYNTAX]
+
+//     default:
+//         break;
+// }
+
+
+const month = 3
+
+// ( shift + alt + downArrow ) to duplicate 
+
+switch (month) {
+    case 1:
+        console.log("January")
+        break;
+    case 2:
+        console.log("Februayy")
+        break;
+    case 3:
+        console.log("March")
+        break;
+    case 4:
+        console.log("April")
+        break;
+
+    default:
+        console.log("default case");
+        break;
+}
+
+// If no Break all the cases will be checked except DEFAULT even after any case gets match 
+
+
