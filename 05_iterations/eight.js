595c4e307b2de8df7fbee309ad4cb991cd792ee4
Sukhwinder Singh <134933892+Sukhwinder-i0@users.noreply.github.com>
2024-06-03 18:59:50 +0000

    dom

--- 05_iterations/eight.js
+++ 05_iterations/eight.js
@@ -1,2 +1,38 @@
 // ********* REDUCE ************//
 
+const myNums = [1, 2, 3, 4]
+
+// const myTotal = myNums.reduce( function (accumulator, curr_value) {
+//     console.log(`Accumulator: ${accumulator} and Current Value: ${curr_value}`);
+//     return accumulator + curr_value;
+// }, 0)
+
+
+// if accumulator ia not passed the first value of arrray will be taken as accumulator 
+
+const myTotal = myNums.reduce( (acc, curr) => acc + curr, 0)
+
+console.log(myTotal);
+
+const shoppingCart = [
+    {
+        itemName: "Js Course",
+        price: 3999
+    },
+    {
+        itemName: "Web Development Course",
+        price: 6999
+    },
+    {
+        itemName: "Mobile development Course",
+        price: 9999
+    },
+    {
+        itemName: "Data Science Course",
+        price: 12999
+    }
+]
+
+const finalPrice = shoppingCart.reduce( (acc, item) => acc + item.price,0 )
+
+console.log(finalPrice);
